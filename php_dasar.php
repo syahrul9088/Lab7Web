7b0a55fb11836930297f47ebcb7e8b92223de7f6
syahrul9088 <39154644+syahrul9088@users.noreply.github.com>
2021-05-10 14:56:20 +0700

    Add files via upload

---
+++ php_dasar.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP Dasar</title>
+</head>
+<body>
+    <h1>Belajar PHP Dasar</h1>
+
+    <?php
+        echo "Hello World". "<br>";
+    ?>
+
+    <h1>Menggunakan Variable</h1>
+
+    <?php
+        $nim = "0411500400";
+        $nama = 'Abdullah';
+        echo "NIM : " . $nim . "<br>";
+        echo "Nama : $nama";
+    ?>
+
+    <?php
+        echo 'Selamat Datang ' . $_GET['nama'];
+    ?>
+
+</body>
+</html>
